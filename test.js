982103ca2263bfbcce980446f08687d0ca5de2ac
simon <simonpavlov2013@gmail.com>
2016-07-13 16:59:31 +0300

    add receive message

--- test.js
+++ test.js
@@ -1,17 +1,18 @@
 var http = require("http");
+var url = require("url");
 var PORT = 7453;
 
 var server = http.createServer();
 server.on("request", function(request, response){
-    console.log(request.method);
-
     switch (request.method) {
         case 'HEAD':
+            console.log('HEAD');
             response.writeHead(200);
             response.write('HEAD response');
             response.end();
             break
         case 'GET':
+            console.log('GET');
             var coordinate = GETRequestParse(request.url);
             var responseText = procRequest(coordinate);
             if(responseText != null){
@@ -23,29 +24,39 @@ server.on("request", function(request, response){
                 response.end();
             }
             break;
+        case 'POST':
+            console.log('POST');
+
+            var data = [];
+            request.on('data', function(chunk) {
+                data.push(chunk);
+            }).on('end', function() {
+                data = Buffer.concat(data).toString();
+                tasks = JSON.parse(data);
+                console.log("data: " + data);
+                console.log("tasks: ");
+                tasks.forEach(function(task) { console.log(task); });
+                response.writeHead(200);
+                response.end();
+            });
+
+            break;
         default:
+            console.log('request.method: ' + request.method);
             response.writeHead(501);
             response.end();
     }
 });
 
-function GETRequestParse(url) {
-    console.log("GET request: " + url);
-    res = {};
-    paramsObj = {};
-    var params = url.split('?')[1].split('&');
-    params.forEach(function(param){
-        param = param.split('=');
-        console.log(param);
-        if(param[0] != 'lat' && param[0] != 'lng') {
-            console.log('***');
-            res = null;
+function GETRequestParse(urlRequest) {
+    console.log("GET request: " + urlRequest);
+    res = url.parse(urlRequest, true).query;
+    for(key in res) {
+        if (["lat", "lng"].indexOf(key) == -1) {
+            console.log("Bad request");
+            return null;
         }
-        paramsObj[param[0]] = +param[1];
-    });
-    console.log(JSON.stringify(paramsObj));
-    if(res != null) res = paramsObj;
-
+    }
     return res;
 }
 
@@ -59,14 +70,14 @@ function procRequest(coordinate) {
         name: "Test ATC",
         perimeter: [
             {lat: 59.89873582670883, lng: 30.264046601951126},
-			{lat: 59.90170036428617, lng: 30.269991047680378},
-			{lat: 59.904817626983764, lng: 30.26839211583138},
-			{lat: 59.90707758975418, lng: 30.259357094764713},
-			{lat: 59.90601260377334, lng: 30.256667844951153},
-			{lat: 59.90298674767275, lng: 30.25417808443308},
-			{lat: 59.90122941209721, lng: 30.250471271574497},
-			{lat: 59.899861914715544, lng: 30.251800306141376},
-			{lat: 59.90001290883071, lng: 30.258453860878944}
+            {lat: 59.90170036428617, lng: 30.269991047680378},
+            {lat: 59.904817626983764, lng: 30.26839211583138},
+            {lat: 59.90707758975418, lng: 30.259357094764713},
+            {lat: 59.90601260377334, lng: 30.256667844951153},
+            {lat: 59.90298674767275, lng: 30.25417808443308},
+            {lat: 59.90122941209721, lng: 30.250471271574497},
+            {lat: 59.899861914715544, lng: 30.251800306141376},
+            {lat: 59.90001290883071, lng: 30.258453860878944}
         ],
         drones: [
             {id: "0x8f6df6aa123", homebase: {lat: 59.903322, lng: 30.267378}, status: "AVAILABLE"},
